5e8e3903d80d252201907fe460a1394481c68f0b
qsalome <qsalome.astro@gmail.com>
2026-05-19 19:07:19 +0300

    Correct type annotation stations folder

--- backend/weather/stations/download_and_parse_mf_pdf.py
+++ backend/weather/stations/download_and_parse_mf_pdf.py
@@ -13,6 +13,7 @@ import pdfplumber
 import requests
 from pdfminer.pdfexceptions import PDFException
 from pdfplumber.utils.exceptions import PdfminerException
+from rest_framework.response import Response
 
 logging.basicConfig(
     level=logging.INFO,
@@ -211,11 +212,11 @@ def get_meteofrance_data_dict(
     return data_dict
 
 
-def get_pdf(pdf_url: str):
+def get_pdf(pdf_url: str) -> Response:
     return requests.get(pdf_url)
 
 
-def save_pdf(filename: Path, pdf_resp) -> None:
+def save_pdf(filename: Path, pdf_resp: Response) -> None:
     with open(filename, "wb") as f:
         f.write(pdf_resp.content)
 
